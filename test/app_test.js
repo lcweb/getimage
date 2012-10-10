fd8188059f685bdc6c64315740a9636ca5af1e48
and <and@nexus20.(none)>
2012-10-10 09:43:56 +0200

    w

---
+++ test/app_test.js
@@ -0,0 +1,42 @@
+var app = require('../app.js').app;
+var supertest = require('supertest');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['awesome'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+
+    supertest(app)
+      .get('/tiger+woods')
+      .expect(302)
+      .end(function (err, res) {
+        if (err) {
+          throw err;
+        }
+        test.done();
+      });
+
+  }
+};
